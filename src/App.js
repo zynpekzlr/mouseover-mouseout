be05cb4fdb31b792c996e4cdce349bf5ea130f32
zynpekzlrt <zynpekzlr@gmail.com>
2024-03-27 19:07:39 +0300

    mouseover-mouseout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+const container = document.getElementById('container')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseout', () => removeColor(square))
+
+  container.appendChild(square)
 }
 
-export default App;
+function setColor(div) {
+  //console.log(div)
+  const randomColor = getRandomColor()
+  div.style.backgroundColor = randomColor
+  div.style.boxShadow = `0 0 2px ${randomColor}, 0 0 10px ${randomColor}`
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function removeColor(div) {
+  //console.log('remove')
+  div.style.backgroundColor = '#111'
+  div.style.boxShadow = '0 0 2px #000'
+}
